5a6ad2c347d8ddd1504c61181aa3470177752851
Aaron Jones <ajones@ulsinc.com>
2020-08-10 11:31:11 -0700

    Remove temporary WIN32 code; use definition from libjpeg instead. Define separate JPEG_LIBRARY_DEBUG/JPEG_LIBRARY_RELEASE locations.

--- cmake-windows.bat
+++ cmake-windows.bat
@@ -4,9 +4,10 @@ cd build
 cmake -G "Visual Studio 15 2017"^
   -Wno-dev^
   -DOpenCV_ROOT:PATH="D:/working_copies/LSM/lsm-10/third-party/install"^
-  -DLibUSB_ROOT:PATH="C:/local/libusb-1.0.23"^
-  -DJPEG_INCLUDE_DIR:PATH="C:/local/libjpeg-turbo/include"^
-  -DJPEG_LIBRARY:FILEPATH="C:/local/libjpeg-turbo/lib/libjpeg-turbod.lib"^
+  -DLibUSB_ROOT:PATH="D:/working_copies/LSM/lsm-10/third-party/install"^
+  -DJPEG_INCLUDE_DIRS:PATH="D:/working_copies/LSM/lsm-10/third-party/install"^
+  -DJPEG_LIBRARY_RELEASE:FILEPATH="D:/working_copies/LSM/lsm-10/third-party/install/lib/jpeg-static.lib"^
+  -DJPEG_LIBRARY_DEBUG:FILEPATH="D:/working_copies/LSM/lsm-10/third-party/install/lib/jpeg-staticd.lib"^
   -DCMAKE_CXX_STANDARD=17^
   -DBUILD_SHARED_LIBS=OFF^
   -DENABLE_UVC_DEBUGGING=ON^
--- src/frame-mjpeg.cpp
+++ src/frame-mjpeg.cpp
@@ -122,46 +122,6 @@ static void insert_huff_tables(j_decompress_ptr dinfo) {
   COPY_HUFF_TABLE(dinfo, ac_huff_tbl_ptrs[1], ac_chromi);
 }
 
-#if _WIN32
-#include <jerror.h> 
-/* Read JPEG image from a memory segment */
-static void init_source (j_decompress_ptr cinfo) {}
-static boolean fill_input_buffer (j_decompress_ptr cinfo)
-{
-    ERREXIT(cinfo, JERR_INPUT_EMPTY);
-return TRUE;
-}
-static void skip_input_data (j_decompress_ptr cinfo, long num_bytes)
-{
-    struct jpeg_source_mgr* src = (struct jpeg_source_mgr*) cinfo->src;
-
-    if (num_bytes > 0) {
-        src->next_input_byte += (size_t) num_bytes;
-        src->bytes_in_buffer -= (size_t) num_bytes;
-    }
-}
-static void term_source (j_decompress_ptr cinfo) {}
-static void jpeg_mem_src (j_decompress_ptr cinfo, const unsigned char* buffer, long nbytes)
-{
-    struct jpeg_source_mgr* src;
-
-    if (cinfo->src == NULL) {   /* first time for this JPEG object? */
-        cinfo->src = (struct jpeg_source_mgr *)
-            (*cinfo->mem->alloc_small) ((j_common_ptr) cinfo, JPOOL_PERMANENT,
-            sizeof(struct jpeg_source_mgr));
-    }
-
-    src = (struct jpeg_source_mgr*) cinfo->src;
-    src->init_source = init_source;
-    src->fill_input_buffer = fill_input_buffer;
-    src->skip_input_data = skip_input_data;
-    src->resync_to_restart = jpeg_resync_to_restart; /* use default method */
-    src->term_source = term_source;
-    src->bytes_in_buffer = nbytes;
-    src->next_input_byte = (JOCTET*)buffer;
-}
-#endif
-
 static uvc_error_t uvc_mjpeg_convert(uvc_frame_t *in, uvc_frame_t *out) {
   struct jpeg_decompress_struct dinfo;
   struct error_mgr jerr;
