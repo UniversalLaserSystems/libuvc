861c1b0c83e7d4ab6a5a411cbcbc317e917af683
Tim Crews <tim@code-affinity.com>
2024-03-27 14:27:16 -0400

    Fix another compilation error

--- src/device.cpp
+++ src/device.cpp
@@ -403,9 +403,9 @@ uvc_error_t uvc_get_device_info(uvc_device_t *dev,
     && !internal_info->config->interface[1].altsetting[0].extra)
   {
     internal_info->config->interface[1].altsetting[0].extra
-      = internal_info->config->interface[1].altsetting[0].endpoint.extra;
+      = internal_info->config->interface[1].altsetting[0].endpoint[0].extra;
     internal_info->config.interface[1]->altsetting[0].extra_length
-      = internal_info->config->interface[1].altsetting[0].endpoint.extra_length;
+      = internal_info->config->interface[1].altsetting[0].endpoint[0].extra_length;
   }
 
   ret = uvc_scan_control(dev, internal_info);
